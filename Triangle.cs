5c8965547c0ca91f53253085f6a117c9ddd010a4
Ruthesh123 <radla2720@conestogac.on.ca>
2025-02-06 22:59:14 -0500

    Add project files.

---
+++ Triangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace IsoscelesTriangle
+{
+    // Author: Ruthesh Reddy Adla
+    public static class Triangle
+    {
+        public static bool IsIsosceles(int a, int b, int c)
+        {
+            Console.WriteLine("Method executed by: Ruthesh Reddy Adla");
+            return (a == b || b == c || a == c);
+        }
+    }
+}
